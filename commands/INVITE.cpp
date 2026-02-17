b7eeec8f0e6eff60343ec166582bba8a43ad282d
Zoe Murie <zmurie@f2r7s6.clusters.42paris.fr>
2026-02-17 14:55:43 +0100

    o

--- commands/INVITE.cpp
+++ commands/INVITE.cpp
@@ -1,6 +1,24 @@
+#include "Server.hpp"
+#include "Client.hpp"
+#include "errors.hpp"
+#include "replies.hpp"
 #include "../include/Commands.hpp"
 
-void Commands::INVITE(Server *server, int fd, std::string user)
+void Commands::INVITE(Server *server, int fd, std::string arg)
 {
+    std::string channel_name;
+    std::string nickname;
+    Client *client = server->getClient(fd);
+    if (channel_name.empty() || nickname.empty())
+    {
+        server->sendMessage(ERR_NEEDMOREPARAMS("INVITE"), fd);
+        return ;
+    }
+    Client *client2 = server->getClient(nickname);
+    if (!client2)
+    {
+        server->sendMessage(ERR_NOSUCHNICK(client->getNickname(), nickname), fd);
+    }
+
 
 }
