b0699469924c7b6a7763c140ea9e602eb7cb837c
Zoe Murie <zmurie@f2r7s6.clusters.42paris.fr>
2026-02-18 08:52:09 +0100

    o

--- commands/MODE.cpp
+++ commands/MODE.cpp
@@ -28,6 +28,68 @@ void Commands::MODE(Server *server, int fd, std::string arg)
     }
     if (name[0] == '#' || name[0] == '&')
     {
+        Channel *channel = server->getChannel(name);
+        if (!channel)
+        {
+            server->sendMessage(ERR_NOSUCHCHANNEL(client->getNickname(), name), fd);
+            return;
+        }
+        if (!channel->isClient(fd))
+        {
+            server->sendMessage(ERR_NOTONCHANNEL(client->getNickname(), name), fd);
+            return;
+        }
+        if (!channel->isOperator(fd))
+        {
+            server->sendMessage(ERR_CHANOPRIVSNEEDED(client->getNickname(), name), fd);
+            return;
+        }
+        
+        if (flags[0] == '-')
+        {
+            for (int i = i; i < flags.size(); i++)
+            {
+                if (flags[i] == 'i')
+                {
+
+                }
+                else if (flags[i] == 't')
+                {
+
+                }
+                else if (flags[i] == 'k')
+                {
+
+                }
+                else if (flags[i] == 'o')
+                {
+
+                }
+                else if (flags[i] == 'l')
+                {
+
+                }
+                else
+                {
+                    server->sendMessage(ERR_UNKNOWNMODE(client->getNickname(), flags[i]), fd);
+                    return;
+                }
+
+            }
+
+        }
+        if (flags[0] == '+')
+        {
+            for (int i = i; i < flags.size(); i++)
+            {
+                
+            }
+        }
+        else 
+        {
+            server->sendMessage(RPL_MODE(client->getNickname(),client->getUsername(), name, channel->getModes()), fd);
+            return;
+        }
 
     }
     else
