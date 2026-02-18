c50142b0de6180c666eccbda42febb423e7a88aa
Zoe Murie <zmurie@f2r7s6.clusters.42paris.fr>
2026-02-18 10:36:54 +0100

    o

--- commands/MODE.cpp
+++ commands/MODE.cpp
@@ -19,6 +19,9 @@ void Commands::MODE(Server *server, int fd, std::string arg)
 {
     std::string name;
     std::string flags;
+    std::string third_arg;
+    bool is_add = false;
+    bool is_del = false;
     Client *client = server->getClient(fd);
 
     if (name.empty() || flags.empty())
@@ -44,25 +47,80 @@ void Commands::MODE(Server *server, int fd, std::string arg)
             server->sendMessage(ERR_CHANOPRIVSNEEDED(client->getNickname(), name), fd);
             return;
         }
-        
-        if (flags[0] == '-')
+        if (flags.empty())
         {
-            for (int i = i; i < flags.size(); i++)
+            server->sendMessage(RPL_MODE(client->getNickname(),client->getUsername(), name, channel->getModes()), fd);
+            return ;
+        }
+        for (int i = 0; i < flags.size(); i++)
             {
-                if (flags[i] == 'i')
+                if (flags[i] == '+')
                 {
-
+                    is_add = true;
+                    is_del = false;
+                }
+                else if (flags[i] == '-')
+                {
+                    is_add = false;
+                    is_del = true;
+                }
+                else if (flags[i] == 'i')
+                {
+                    if (is_add)
+                        channel->setInviteOnly(true);
+                    else if (is_del)
+                        channel->setInviteOnly(false);
                 }
                 else if (flags[i] == 't')
                 {
-
+                    if (is_add)
+                        channel->setTopicRestricted(true);
+                    else if (is_del)
+                        channel->setTopicRestricted(false);
                 }
                 else if (flags[i] == 'k')
                 {
-
+                    if (is_add && !third_arg.empty())
+                    {
+                        channel->setPwd(third_arg);
+                        channel->setHasModeK(true);
+                    }
+                    else if (is_add && third_arg.empty())
+                    {
+                        server->sendMessage(ERR_NEEDMOREPARAMS("MODE"), fd);
+                        continue ;
+                    }
+                    else if (is_del)
+                    {
+                        channel->setHasModeK(false);
+                    }
                 }
                 else if (flags[i] == 'o')
                 {
+                    if ((is_add || is_del) && third_arg.empty())
+                    {
+                        server->sendMessage(ERR_NEEDMOREPARAMS("MODE"), fd);
+                        continue;
+                    }
+                    else if (third_arg.empty())
+                    {
+                        server->sendMessage(RPL_MODE(client->getNickname(),client->getUsername(), name, channel->getModes()), fd);
+                        continue;
+                    }
+                    if (!channel->isClient(third_arg))
+                    {
+                        server->sendMessage(ERR_NOTONCHANNEL(client->getNickname(), third_arg), fd);
+                        continue;
+                    }
+                    if (is_add)
+                    {
+                        channel->addOperator(third_arg);
+                    }
+
+                    else if (is_del)
+                    {
+                        channel->delOperator(third_arg);
+                    }
 
                 }
                 else if (flags[i] == 'l')
@@ -72,18 +130,11 @@ void Commands::MODE(Server *server, int fd, std::string arg)
                 else
                 {
                     server->sendMessage(ERR_UNKNOWNMODE(client->getNickname(), flags[i]), fd);
-                    return;
+                    continue;
                 }
 
             }
 
-        }
-        if (flags[0] == '+')
-        {
-            for (int i = i; i < flags.size(); i++)
-            {
-                
-            }
         }
         else 
         {
