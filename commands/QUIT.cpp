d5a1079cc386f9a82dd6151768075d7883c74ebe
Zoe Murie <zmurie@f4r4s1.clusters.42paris.fr>
2026-02-21 19:36:53 +0100

    o

--- commands/QUIT.cpp
+++ commands/QUIT.cpp
@@ -5,10 +5,17 @@
 void Commands::QUIT(Server *server, int fd, std::string reason)
 {
     Client *client = server->getClient(fd);
+    std::map<std::string, Channel*> channels = server->getChannels();
     if (reason.size() == 0)
-        reason = "Leaving";
-    server->sendMessage(MSG_QUIT(client->getNickname() ,reason), fd);
-    // ajouter client deconnecte au servuer
-
-    return;
+        reason = "Leaving";   
+    for (std::map<std::string, Channel*>::iterator it = channels.begin(); it != channels.end(); it++)
+    {
+        Channel *channel = it->second;
+        if (channel->isClient(fd))
+        {
+            channel->broadcast(MSG_QUIT(client->getNickname() ,reason), -1);
+            channel->delUser(client->getNickname());
+        }
+    }
+    server->clientDisconnect(fd);
 }
