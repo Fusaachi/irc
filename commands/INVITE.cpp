31dc8abeb09728882dceeb8d8f2b66c661cf90a9
Zoe Murie <zmurie@f2r11s6.clusters.42paris.fr>
2026-02-25 17:04:55 +0100

    o

--- commands/INVITE.cpp
+++ commands/INVITE.cpp
@@ -60,7 +60,8 @@ void Commands::INVITE(Server *server, int fd, std::string arg)
             return ;
         }
     }
-    channel->inviteClient(client2->getFd());
+    if (channel->hasModeI())
+        channel->inviteClient(client2->getFd());
     server->sendMessage(RPL_NOTIFICATION(client->getNickname(), client->getUsername(), nickname,channel_name), client2->getFd());
     server->sendMessage(RPL_INVITING(client->getNickname(), nickname, channel_name), fd);
 
