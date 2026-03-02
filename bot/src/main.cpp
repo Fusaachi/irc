fa3c5c4e39506523188f5e35629ea3808ee7aa8d
pfranke <pfranke@student.42.fr>
2026-03-02 16:28:47 +0100

    bot leaks

--- bot/src/main.cpp
+++ bot/src/main.cpp
@@ -6,13 +6,19 @@
 /*   By: pfranke <pfranke@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 17:16:43 by pfranke           #+#    #+#             */
-/*   Updated: 2026/03/02 16:23:50 by pfranke          ###   ########.fr       */
+/*   Updated: 2026/03/02 16:27:16 by pfranke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Bot.hpp"
 
-int main(int argc, char **argv)
+static volatile sig_atomic_t g_signal_received = 0;
+
+void signal_handler(int signal)
+{
+	g_signal_received = signal;
+}
+int main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
@@ -20,9 +26,10 @@ int main(int argc, char **argv)
 		return 1;
 	}
 	
+	signal(SIGINT, signal_handler);	
 	Bot bot("QuoicouBot");
 	bot.ConnectToServer(std::atoi(argv[1]), argv[2]);
 	bot.ConnectToChannel("#feur");
-	while (true) 
+	while (g_signal_received == 0) 
 		bot.LoopEvents();
 }
